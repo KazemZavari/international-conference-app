a40325c474fa22cd0d80c78c4b8dc50318abc7db
kazem <kazemzavari.69@gmail.com>
2024-03-02 02:32:14 +0330

    add animation to MainTopics

--- src/components/MainTopics.tsx
+++ src/components/MainTopics.tsx
@@ -1,7 +1,7 @@
 import Divider from "../components/Divider";
 import ColorDiv from "../components/ColorDiv";
 import img from "../assets/img/pic-7.jpg";
-import { AnimatePresence, motion } from "framer-motion";
+
 const MainTopics = () => {
   return (
     <div className="bg-[#114b5f] h-[180vh] inlin-flex justify-center relative overflow-hidden">
@@ -12,12 +12,12 @@ const MainTopics = () => {
           <img className="h-[16.5rem] rounded-t-2xl" src={img} />
           <div className="group ">
             <div
-              className="group-hover:animate-in group-hover:slide-in-from-bottom-10 group-hover:duration-1000 
+              className="group-hover:animate-in group-hover:slide-in-from-bottom-[4rem] group-hover:duration-1000 
             animate-out slide-out-to-top duration-1000"
             >
               <div
                 className="bg-white/50 w-[100%] hidden group-hover:block group-hover:absolute -mt-[17rem]
-                       h-[18rem] animate-out duration-1000  "
+                       h-[18rem] "
               >
                 <h3 className="font-semibold text-[2rem] mt-2 "> title </h3>
                 <ul>
