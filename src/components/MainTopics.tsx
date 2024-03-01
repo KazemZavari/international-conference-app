73b88b18964d6bb8b502fc030e272cf379a27e69
kazem <kazemzavari.69@gmail.com>
2024-03-02 02:23:10 +0330

    add animation to MainTopics

--- src/components/MainTopics.tsx
+++ src/components/MainTopics.tsx
@@ -1,27 +1,34 @@
 import Divider from "../components/Divider";
 import ColorDiv from "../components/ColorDiv";
 import img from "../assets/img/pic-7.jpg";
+import { AnimatePresence, motion } from "framer-motion";
 const MainTopics = () => {
   return (
     <div className="bg-[#114b5f] h-[180vh] inlin-flex justify-center relative overflow-hidden">
       <ColorDiv />
       <Divider borderText="Main Topics of The Conference" />
       <div className="mt-[1rem] grid grid-cols-4 gap-5 mx-28  ">
-        <div className="col-span-1 bg-white h-[26rem] w-[100%] rounded-3xl mt-8 relative overflow-hidden ">
+        <div className="col-span-1 bg-white  h-[26rem] w-[100%] rounded-3xl mt-8 relative overflow-hidden ">
           <img className="h-[16.5rem] rounded-t-2xl" src={img} />
-          <div className="group">
+          <div className="group ">
             <div
-              className="bg-white w-[100%] -mt-[17rem] hidden group-hover:block group-hover:absolute 
-                        h-[20rem] transition-transform duration-1000 z-100 delay-700 "
+              className="group-hover:animate-in group-hover:slide-in-from-bottom-10 group-hover:duration-1000 
+            animate-out slide-out-to-top duration-1000"
             >
-              <h3 className="font-semibold text-[2rem] "> title </h3>
-              <ul>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-              </ul>
+              <div
+                className="bg-white/50 w-[100%] hidden group-hover:block group-hover:absolute -mt-[17rem]
+                       h-[18rem] animate-out duration-1000  "
+              >
+                <h3 className="font-semibold text-[2rem] mt-2 "> title </h3>
+                <ul>
+                  <li>text</li>
+                  <li>text</li>
+                  <li>text</li>
+                  <li>text</li>
+                </ul>
+              </div>
             </div>
+
             <h3 className="font-semibold text-[2rem] group-hover:opacity-0 ">
               {" "}
               title{" "}
@@ -30,175 +37,7 @@ const MainTopics = () => {
 
           <button
             className="bg-teal-600 hover:bg-cyan-900 px-4 pt-2 pb-3 rounded-3xl mt-10 text-white 
-                            font-semibold transition-all duration-300 "
-          >
-            Reade more
-          </button>
-        </div>
-        <div className="col-span-1 bg-white h-[26rem] w-[100%] rounded-3xl mt-8 relative overflow-hidden ">
-          <img className="h-[16.5rem] rounded-t-2xl" src={img} />
-          <div className="group">
-            <div
-              className="bg-white w-[100%] -mt-[17rem] hidden group-hover:block group-hover:absolute 
-                        h-[20rem] transition-transform duration-1000 z-100 delay-700 "
-            >
-              <h3 className="font-semibold text-[2rem] "> title </h3>
-              <ul>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-              </ul>
-            </div>
-            <h3 className="font-semibold text-[2rem] group-hover:opacity-0 ">
-              {" "}
-              title{" "}
-            </h3>
-          </div>
-
-          <button
-            className="bg-teal-600 hover:bg-cyan-900 px-4 pt-2 pb-3 rounded-3xl mt-10 text-white 
-                            font-semibold transition-all duration-300 "
-          >
-            Reade more
-          </button>
-        </div>
-        <div className="col-span-1 bg-white h-[26rem] w-[100%] rounded-3xl mt-8 relative overflow-hidden ">
-          <img className="h-[16.5rem] rounded-t-2xl" src={img} />
-          <div className="group">
-            <div
-              className="bg-white w-[100%] -mt-[17rem] hidden group-hover:block group-hover:absolute 
-                        h-[20rem] transition-transform duration-1000 z-100 delay-700 "
-            >
-              <h3 className="font-semibold text-[2rem] "> title </h3>
-              <ul>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-              </ul>
-            </div>
-            <h3 className="font-semibold text-[2rem] group-hover:opacity-0 ">
-              {" "}
-              title{" "}
-            </h3>
-          </div>
-
-          <button
-            className="bg-teal-600 hover:bg-cyan-900 px-4 pt-2 pb-3 rounded-3xl mt-10 text-white 
-                            font-semibold transition-all duration-300 "
-          >
-            Reade more
-          </button>
-        </div>
-        <div className="col-span-1 bg-white h-[26rem] w-[100%] rounded-3xl mt-8 relative overflow-hidden ">
-          <img className="h-[16.5rem] rounded-t-2xl" src={img} />
-          <div className="group">
-            <div
-              className="bg-white w-[100%] -mt-[17rem] hidden group-hover:block group-hover:absolute 
-                        h-[20rem] transition-transform duration-1000 z-100 delay-700 "
-            >
-              <h3 className="font-semibold text-[2rem] "> title </h3>
-              <ul>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-              </ul>
-            </div>
-            <h3 className="font-semibold text-[2rem] group-hover:opacity-0 ">
-              {" "}
-              title{" "}
-            </h3>
-          </div>
-
-          <button
-            className="bg-teal-600 hover:bg-cyan-900 px-4 pt-2 pb-3 rounded-3xl mt-10 text-white 
-                            font-semibold transition-all duration-300 "
-          >
-            Reade more
-          </button>
-        </div>
-        <div className="col-span-1 bg-white h-[26rem] w-[100%] rounded-3xl mt-8 relative overflow-hidden ">
-          <img className="h-[16.5rem] rounded-t-2xl" src={img} />
-          <div className="group">
-            <div
-              className="bg-white w-[100%] -mt-[17rem] hidden group-hover:block group-hover:absolute 
-                        h-[20rem] transition-transform duration-1000 z-100 delay-700 "
-            >
-              <h3 className="font-semibold text-[2rem] "> title </h3>
-              <ul>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-              </ul>
-            </div>
-            <h3 className="font-semibold text-[2rem] group-hover:opacity-0 ">
-              {" "}
-              title{" "}
-            </h3>
-          </div>
-
-          <button
-            className="bg-teal-600 hover:bg-cyan-900 px-4 pt-2 pb-3 rounded-3xl mt-10 text-white 
-                            font-semibold transition-all duration-300 "
-          >
-            Reade more
-          </button>
-        </div>
-        <div className="col-span-1 bg-white h-[26rem] w-[100%] rounded-3xl mt-8 relative overflow-hidden ">
-          <img className="h-[16.5rem] rounded-t-2xl" src={img} />
-          <div className="group">
-            <div
-              className="bg-white w-[100%] -mt-[17rem] hidden group-hover:block group-hover:absolute 
-                        h-[20rem] transition-transform duration-1000 z-100 delay-700 "
-            >
-              <h3 className="font-semibold text-[2rem] "> title </h3>
-              <ul>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-              </ul>
-            </div>
-            <h3 className="font-semibold text-[2rem] group-hover:opacity-0 ">
-              {" "}
-              title{" "}
-            </h3>
-          </div>
-
-          <button
-            className="bg-teal-600 hover:bg-cyan-900 px-4 pt-2 pb-3 rounded-3xl mt-10 text-white 
-                            font-semibold transition-all duration-300 "
-          >
-            Reade more
-          </button>
-        </div>
-        <div className="col-span-1 bg-white h-[26rem] w-[100%] rounded-3xl mt-8 relative overflow-hidden ">
-          <img className="h-[16.5rem] rounded-t-2xl" src={img} />
-          <div className="group">
-            <div
-              className="bg-white w-[100%] -mt-[17rem] hidden group-hover:block group-hover:absolute 
-                        h-[20rem] transition-transform duration-1000 z-100 delay-700 "
-            >
-              <h3 className="font-semibold text-[2rem] "> title </h3>
-              <ul>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-                <li>text</li>
-              </ul>
-            </div>
-            <h3 className="font-semibold text-[2rem] group-hover:opacity-0 ">
-              {" "}
-              title{" "}
-            </h3>
-          </div>
-
-          <button
-            className="bg-teal-600 hover:bg-cyan-900 px-4 pt-2 pb-3 rounded-3xl mt-10 text-white 
-                            font-semibold transition-all duration-300 "
+                            font-semibold transition-all duration-300 z-1000 "
           >
             Reade more
           </button>
