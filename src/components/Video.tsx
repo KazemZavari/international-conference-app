74e1aec2316a6c21f6e8ec3e385d20502d07fdf5
kazem <kazemzavari.69@gmail.com>
2024-03-24 22:38:15 +0330

    test pic

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,7 +2,7 @@ import video from "../assets/bgVideo/earth-1.mp4";
  import pic from "../assets/img/pic-1.jpg"
 const Video = () => {
   return (
-    <div className={`absolute -z-10 h-[100vh] overflow-hidden  ] `}>
+    <div className={`absolute -z-10 h-[100vh] overflow-hidden] `}>
     <video
       className="w-[100vw]"
       autoPlay
@@ -10,7 +10,7 @@ const Video = () => {
       playsInline
       loop
       src={video}
-    ></video>
+    ></video> 
     <img  src={pic} />
   </div>
   )
